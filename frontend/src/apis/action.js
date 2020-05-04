ad7484a27cdc8e97dcfaf6c9bea63bd1510a7be4
Chen <victorchennn@gmail.com>
2020-05-04 16:13:26 -0400

    add search ways

--- frontend/src/apis/action.js
+++ frontend/src/apis/action.js
@@ -3,5 +3,5 @@ import axios from "axios";
 var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 
 export default axios.create({
-  baseURL: proxyUrl + "https://stub-dot-en-601-666.ue.r.appspot.com"
+  baseURL: proxyUrl + "https://stub-dot-en-601-666.ue.r.appspot.com" // /{... bm25/tfidf/bert}
 });
